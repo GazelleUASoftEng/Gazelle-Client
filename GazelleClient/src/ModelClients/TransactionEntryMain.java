d44a928cb2aa7a2ab245a598e4ad95b27f30447c
ThyRex3 <kathydai@email.uark.edu>
2015-10-30 17:49:52 -0500

    Removed unnecessary import statements

--- GazelleClient/src/ModelClients/TransactionEntryMain.java
+++ GazelleClient/src/ModelClients/TransactionEntryMain.java
@@ -1,8 +1,6 @@
 package ModelClients;
 
 import org.gazelle.models.TransactionEntry;
-import org.gazelle.models.Transaction;
-import org.gazelle.models.Product;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -10,8 +8,8 @@ public class TransactionEntryMain {
 
 	public static void main (String[] args) {
 		TransactionEntry testTransactionEntry = new TransactionEntry();
-		testTransactionEntry.setPrice(10.05);
-		testTransactionEntry.setQuantity(5);
+		testTransactionEntry.setPrice(20.02);
+		testTransactionEntry.setQuantity(2);
 		testTransactionEntry.setCreatedOn(LocalDateTime.now());
 		testTransactionEntry.setTransactionID(UUID.fromString("5fa8f1d3-7291-4996-b009-b233ca7cfd18"));
 		testTransactionEntry.setProductID(UUID.fromString("bd7c1334-abfe-4693-bc6a-6fe6bdbbfa38"));
@@ -19,3 +17,4 @@ public class TransactionEntryMain {
 		testTransactionEntry.save();
 	}
 }
+ 
